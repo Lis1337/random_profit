522bd0b821ed690d644aecbfe8fc5770587197e6
ilyalin <i.lyalin@profitbase.ru>
2022-08-07 14:27:43 +0500

    first

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('list/', views.get_users_list, name='list'),
+    path('rounds/', views.get_rounds, name='rounds'),
+    path('pairs/', views.get_pairs, name='pairs'),
+    path('round/make/', views.make_round, name='round_make')
 ]
