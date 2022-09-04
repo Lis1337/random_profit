837f0936f86aa5a63d2d2454f87a80180589a078
ilyalin <i.lyalin@profitbase.ru>
2022-09-04 20:59:47 +0500

    mvp prealfabetagamma

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.get_users_list, name='list'),
-    path('rounds/', views.get_rounds, name='rounds'),
     path('pairs/', views.get_pairs, name='pairs'),
-    path('round/make/', views.make_round, name='round_make')
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import User, Round, Pairs
+from .models import User, Pairs
+from rounds.models import Round
 import random
 
 
@@ -23,44 +24,3 @@ def get_rounds(request):
 def get_pairs(request):
     pairs = Pairs.objects.all()
     return render(request, 'pairs.html', {"pairs": pairs})
-
-
-def make_round(request):
-    users = User.objects.all()
-    participants = users.filter(is_included=1)
-    dated_pairs = Pairs.objects.all()
-
-    next_round = []
-    for user in participants:
-        next_round.append(user.first_name + '_' + user.second_name)
-
-    result = []
-    amount_of_loops = len(next_round) // 2
-    i = 0
-    while i <= amount_of_loops:
-
-        first_user = next_round.pop()
-        second_user = None
-
-        for second in next_round:
-            pairs_exists = (
-                dated_pairs.filter(first_user=first_user, second_user=second).exists() or
-                dated_pairs.filter(first_user=second, second_user=first_user).exists()
-            )
-
-            if ((first_user != second) and (not pairs_exists)):
-                second_user = second
-                break
-
-
-        if ((second_user is not None) and (first_user != second_user)):
-            result.append(first_user + ':' + second_user)
-            next_round.remove(second_user)
-        else:
-            result.append(first_user + ':' + 'Ann_Machneva')
-
-        i += 1
-
-
-
-    return render(request, 'base.html', {"round": result,})
